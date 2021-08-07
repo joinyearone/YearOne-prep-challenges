219a437c4002e6aa8702b548d4667a0e940cc188
Restyled.io <commits@restyled.io>
2021-08-07 02:37:22 +0000

    Restyled by black

--- py_actions.py
+++ py_actions.py
@@ -4,23 +4,25 @@ import os
 from datetime import date
 from pprint import pprint
 
-API_TOKEN = os.environ.get('API_TOKEN')
-COMMUNITY_ID = os.environ.get('COMMUNITY_ID')
-CIRCLE_COMMUNITY_PATH = os.environ.get('CIRCLE_COMMUNITY_PATH')
-DAILY_SPACE_ID = os.environ.get('DAILY_SPACE_ID')
+API_TOKEN = os.environ.get("API_TOKEN")
+COMMUNITY_ID = os.environ.get("COMMUNITY_ID")
+CIRCLE_COMMUNITY_PATH = os.environ.get("CIRCLE_COMMUNITY_PATH")
+DAILY_SPACE_ID = os.environ.get("DAILY_SPACE_ID")
 
 
 def post_to_circle(space_id, title_text, body_message, author_email=None):
     """
     Sends POST request to circle platform, posting new content to the community
-    platform in the designated space. 
+    platform in the designated space.
 
     """
 
     url = f"{CIRCLE_COMMUNITY_PATH}/api/v1/posts?community_id={COMMUNITY_ID}&space_id={space_id}&"
     title = f"name={title_text}&"
     body = f"body={body_message}&"
-    url_ending_params = "is_comments_enabled=true&is_liking_enabled=true&is_truncation_disabled=true"
+    url_ending_params = (
+        "is_comments_enabled=true&is_liking_enabled=true&is_truncation_disabled=true"
+    )
     author = f"&user_email={author_email}"
 
     url_pieces = [url, title, body, url_ending_params]
@@ -31,12 +33,9 @@ def post_to_circle(space_id, title_text, body_message, author_email=None):
     post_url = "".join(url_pieces)
 
     payload = {}
-    headers = {
-        'Authorization': API_TOKEN
-    }
+    headers = {"Authorization": API_TOKEN}
     try:
-        response = requests.request(
-            "POST", post_url, headers=headers, data=payload)
+        response = requests.request("POST", post_url, headers=headers, data=payload)
         return response.status_code
     except Exception as e:
         return f"Error on circle post: {e}"
@@ -47,12 +46,9 @@ def get_post_count(space_id):
     url = f"{CIRCLE_COMMUNITY_PATH}/api/v1/posts?community_id={COMMUNITY_ID}&space_id={space_id}"
 
     payload = {}
-    headers = {
-        'Authorization': API_TOKEN
-    }
+    headers = {"Authorization": API_TOKEN}
     try:
-        circle_api = requests.request(
-            "GET", url, headers=headers, data=payload).json()
+        circle_api = requests.request("GET", url, headers=headers, data=payload).json()
         post_count = len(circle_api)
 
         return post_count
--- tests/py_actions_test.py
+++ tests/py_actions_test.py
@@ -4,13 +4,16 @@ from .. import py_actions
 
 class Test_Py_actions_Get_post_count:
     def test_get_post_count_1(self):
-        result = py_actions.get_post_count(
-            "03ea49f8-1d96-4cd0-b279-0684e3eec3a9")
+        result = py_actions.get_post_count("03ea49f8-1d96-4cd0-b279-0684e3eec3a9")
         assert pytest.raises(Exception)
 
 
 class Test_Py_actions_Post_to_circle:
     def test_post_to_circle_1(self):
         result = py_actions.post_to_circle(
-            "7289708e-b17a-477c-8a77-9ab575c4b4d8", "01:04:03", "cool", "email@Google.com")
+            "7289708e-b17a-477c-8a77-9ab575c4b4d8",
+            "01:04:03",
+            "cool",
+            "email@Google.com",
+        )
         assert pytest.raises(Exception)
