340f255b58510f917888583c8d9609a38ae7781c
Restyled.io <commits@restyled.io>
2021-08-07 02:37:21 +0000

    Restyled by autopep8

--- daily_challenge.py
+++ daily_challenge.py
@@ -4,12 +4,13 @@ import yaml
 
 import py_actions
 
+
 def random_topic_chooser():
     """
     Opens the topics.yaml and randomly chooses a key where the difficulty 
     level is not 'Advanced'. 'Advanced' topics are not sent as a part of the
     daily challenge.
-    
+
     """
 
     def choose_random_topic(data):
@@ -24,7 +25,7 @@ def random_topic_chooser():
         try:
             data = yaml.safe_load(stream)
             topic = choose_random_topic(data)
-            
+
             return data[topic]
         except yaml.YAMLError as exc:
             raise yaml.YAMLError(f"{exc}")
@@ -37,13 +38,13 @@ def format_post_body(topic, post_num):
     👏👏 Thanks to Kristal for the question! 👏👏
     Difficulty Level: Beginner
     Source: Leetcode
-    
+
     """
     try:
         post_num += 1
         topic_title = topic['title']
-        
-        #TODO this formatted string creates an error in the posting, but we should figure it out
+
+        # TODO this formatted string creates an error in the posting, but we should figure it out
         # title = f"Daily Challenge #{post_num}: {topic_title}"
 
         body = topic['body']
@@ -51,12 +52,12 @@ def format_post_body(topic, post_num):
         source = topic['source'] if 'source' in topic else None
         author_email = topic['author_email'] if 'author_email' in topic else None
         author_name = topic['author_name'] if 'author_name' in topic else None
-        
+
         shout = f"<h2>👏👏 Thanks to <u>{author_name}</u> for the question! 👏👏</h2><br>" \
             if author_name else ""
         level = f"<strong>Difficulty Level:</strong> {difficulty}<br>"
         sourced = f"<strong>Sourced from:</strong> {source}<br>" if source else ""
-        
+
         pre_body = [shout, level, sourced, "<br>"]
         end_body = "<br><br> \
                     Don't forget to let us know that you've completed this question!<br> \
@@ -74,14 +75,16 @@ def actions():
     """
     try:
         topic = random_topic_chooser()
-        
+
         post_num = py_actions.get_post_count(py_actions.DAILY_SPACE_ID)
 
         title, post_body, author_email = format_post_body(topic, post_num)
-        
-        py_actions.post_to_circle(py_actions.DAILY_SPACE_ID, title, post_body, author_email)
-                    
+
+        py_actions.post_to_circle(
+            py_actions.DAILY_SPACE_ID, title, post_body, author_email)
+
     except Exception as e:
         return e
+
 
 print(actions())
