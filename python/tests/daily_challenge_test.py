b7c1812e6a45ac24a9f912d1e2dd1a37853d5a4f
Restyled.io <commits@restyled.io>
2021-08-07 04:27:35 +0000

    Restyled by autopep8

--- python/tests/daily_challenge_test.py
+++ python/tests/daily_challenge_test.py
@@ -1,10 +1,11 @@
 import pytest
 from .. import daily_challenge
 
+
 class Test_Daily_challenge_Format_post_body:
     def test_format_post_body_1(self):
-        result = daily_challenge.format_post_body({ "title": "title", "body": "text body", "author_name": "something@example.com", \
-                                                    "difficulty": True, "author_email": "user@host:300" },5)
+        result = daily_challenge.format_post_body({"title": "title", "body": "text body", "author_name": "something@example.com",
+                                                   "difficulty": True, "author_email": "user@host:300"}, 5)
         title, topic, author = result
         assert isinstance(title, str)
         assert isinstance(topic, str)
@@ -12,5 +13,5 @@ class Test_Daily_challenge_Format_post_body:
         assert all(result)
 
     def test_format_post_body_fail(self):
-        result = daily_challenge.format_post_body({},5)
+        result = daily_challenge.format_post_body({}, 5)
         assert pytest.raises(Exception)
