0c2308462b3b87e2658a20d40aa2fa67a067cfdd
Rafael Castellanos-Welsh <46855493+rafawelsh@users.noreply.github.com>
2021-08-09 17:56:16 -0700

    Feat: add new questions to corresponding difficulty space (#18)
    * Initialized add_challenge_to_space.js
    
    * Initialized add_challenge_to_space.js
    
    * feat: add ability to post questions to correspoding difficulty space
    
    add_challenge_to_space script adds any new questions within topics.yaml to their correspoding difficulty space in circle'
    
    * Initialized add_challenge_to_space.js
    
    * feat: add ability to post questions to correspoding difficulty space
    
    add_challenge_to_space script adds any new questions within topics.yaml to their correspoding difficulty space in circle'
    
    * feat: add new question to correspoding space
    
    add_challenge_to_spaces is checking the new questions and marking them from the YAML file.
    js_actions is adding the questions to the correspoding space.
    
    * refactor: move javascript files into javascript
    
    Co-authored-by: Kristal <kgmajor1@gmail.com>

--- javascript/add_challenge_to_spaces.js
+++ javascript/add_challenge_to_spaces.js
@@ -1,22 +1,22 @@
-// Import dependcies
+//Import dependcies
 const fs = require("fs");
 const YAML = require("js-yaml");
 
-// import actions
+//import actions
 const { postQuestion } = require("./js_actions.js");
 
 try {
-  const raw = fs.readFileSync("./topics.yaml");
-  const data = YAML.load(raw);
-  Object.values(data).forEach((item) => {
-    if (!item.posted) {
-      postQuestion(item);
-      item.posted = true;
-    }
-  });
+	const raw = fs.readFileSync("./topics.yaml");
+	const data = YAML.load(raw);
+	Object.values(data).forEach((item) => {
+		if (!item.posted) {
+			postQuestion(item);
+			item.posted = true;
+		}
+	});
 
-  let newData = YAML.dump(data);
-  fs.writeFileSync("./topics.yaml", newData, "utf8");
+	let newData = YAML.dump(data);
+	fs.writeFileSync("./topics.yaml", newData, "utf8");
 } catch (err) {
-  console.log(err);
+	console.log(err);
 }
