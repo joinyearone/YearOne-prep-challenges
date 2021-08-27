4bad971b3966dfb1c0f43a164894f67b733a9f09
Kristal <kgmajor1@gmail.com>
2021-08-26 22:46:08 -0700

    Update daily challenge (#37)
    * update to binary search
    
    * change test

--- python/daily_challenge.py
+++ python/daily_challenge.py
@@ -1,65 +1,94 @@
-import random
-from pprint import pprint
-
+import os
+import requests
+import json
+import datetime
+import urllib.parse
 import py_actions
 
+BINARYSEARCH_TOKEN = os.environ.get("BINARYSEARCH_TOKEN")
 
-def random_topic_chooser():
-    """
-    Opens the topics.yaml and randomly chooses a key where the difficulty
-    level is not 'Advanced'. 'Advanced' topics are not sent as a part of the
-    daily challenge.
+day_of_week = datetime.date.today().isoweekday()
 
-    """
-    def choose_random_topic(data):
-        topic = random.choice(list(data.keys()))
+def createContest():
+    easy_med = [0,1]
+    med_hard = [2,3]
 
-        if data[topic]["difficulty"] != "Advanced":
-            return topic
-        else:
-            return choose_random_topic(data)
-    try:   
-        data = py_actions.read_topics_yaml()
-        topic_number = choose_random_topic(data)
+    if day_of_week > 3:
+        difficulties = med_hard
+    else:
+        difficulties = easy_med
 
-        return topic_number, data[topic_number]
-    except Exception as e:
-        return e
-
-
-def format_post_body(topic, topic_number):
-    """
-    Format text body to include standardized text:
-    Example:
-    👏👏 Thanks to Kristal for the question! 👏👏
-    Difficulty Level: Beginner
-    Source: Leetcode
-
-    """
+    headers = {
+        "x-access-token": f"{BINARYSEARCH_TOKEN}",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "capacity": 50,
+        "companies": [],
+        "contestSessionId": False,
+        "difficulties": difficulties,
+        "educationalContest": True,
+        "inviteOnly": True,
+        "isPublic": False,
+        "listId": False,
+        "questionIds": [],
+        "questionsPerSession": 4,
+        "time": 28800,
+        "timeMultiple": 1,
+        "topics": []
+    }
+    algoURL = 'https://binarysearch.com/api/rooms'
+    payload = json.dumps(payload)
     try:
-        topic_title = topic["title"]
-        title = f"Daily Challenge #{topic_number}: {topic_title}"
-        body = topic["body"]
-        difficulty = topic["difficulty"]
-        source = topic["source"] if "source" in topic else None
-        author_email = topic[
-            "author_email"] if "author_email" in topic else None
-        author_name = topic["author_name"] if "author_name" in topic else None
+        res = requests.request("POST", algoURL, headers=headers, data=payload).json()
+        return res
+    except Exception as e:
+        return f"Error on pad fetch: {e}"
 
-        shout = (
-            f"<h2>👏👏 Thanks to <u>{author_name}</u> for the question! 👏👏</h2><br>"
-            if author_name else "")
-        level = f"<strong>Difficulty Level:</strong> {difficulty}<br>"
-        sourced = f"<strong>Sourced from:</strong> {source}<br>" if source else ""
 
-        pre_body = [shout, level, sourced, "<br>"]
-        end_body = "<br><br> \
+def format_post_body(contestURL):
+    dt = datetime.datetime.today()
+    date_string = f"{dt.month}/{dt.day}"
+    if day_of_week > 3:
+        difficulty = "Medium & Hard"
+    else:
+        difficulty = "Easy & Medium"
+    try:
+        title = f"Daily Algo Challenge: {date_string}"
+        level = f"<strong>Difficulty Level:</strong> {difficulty}"
+        body = f"""<strong>▶️ Here's today's YearOne Algo Challenge!</strong><br>
+                {level}<br> <br>
+                <h3> 🔗 <a href="{contestURL}" target="_blank">Join the challenge </a></h3>
+                <br>
+               <strong> ✅ Join the room | ✅ Hit 'Ready' | ✅ Begin the challenge!</strong><br>
+                
+                <br>
+                <hr>
+                <p><strong>If you haven't joined before, here's how to participate: </strong><br>
+                <ol>
+                <li> Sign up for an account at <a href="https://binarysearch.com" target="_blank">Binary Search </a></li>
+                <li>Click the link above to join our private challenge room</li>
+                <li>Don't forget to use the voice chat or room chat to see how others are doing!</li>
+                </ol>
+                </p>
+                <br>
+                <p>
+                <strong> FAQs</strong>
+                <br>
+                <ul>
+                <li>How long does the challenge run? It runs for 8 hours, beginning at 8 am Eastern Time </li>
+                <li>How often does it run? Monday to Friday!</li>
+                <li>Is it free? Of course!</li>
+                </ul>
+                </p>"""
+        end_body = "<br> \
                     Don't forget to let us know that you've completed this question!<br> \
                     Leave a comment below 👇👇👇👇"
 
-        post_body = "".join(pre_body) + body + end_body
+        post_body = "".join(body + end_body)
+        encoded_body = urllib.parse.quote(post_body)
 
-        return (title, post_body, author_email)
+        return (title, encoded_body)
     except KeyError as e:
         return e
     except TypeError as f:
@@ -71,14 +100,15 @@ def actions():
     Calls required functions to choose topic, format text body, and post to Circle.
     """
     try:
-        topic_number, topic_data = random_topic_chooser()
+        contest_data = createContest()
 
-        title, post_body, author_email = format_post_body(topic_data, topic_number)
+        room_slug = contest_data['uniqueSlug']
 
-        print(title, post_body, author_email)
+        contestURL = f'https://www.binarysearch.com/room/{room_slug}'
 
-        # py_actions.post_to_circle(py_actions.DAILY_SPACE_ID, title, post_body,
-        #                           author_email)
+        title, post_body = format_post_body(contestURL)
+
+        py_actions.post_to_circle(py_actions.DAILY_SPACE_ID, title, post_body)
 
     except Exception as e:
         return e
