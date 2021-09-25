7a36a59c01b0bfe740094804a5e7cfabba910efa
Kristal <kristal@joinyearone.io>
2021-09-24 21:06:50 -0700

    trying to resolve import error

--- javascript/js_actions.js
+++ javascript/js_actions.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
 const API_TOKEN = process.env.API_TOKEN;
 const COMMUNITY_ID = process.env.COMMUNITY_ID;
@@ -66,4 +66,4 @@ const postQuestion = async (question, questionNumber) => {
 		.catch((error) => console.log("error", error));
 };
 
-module.exports = { postQuestion };
+export default { postQuestion };
